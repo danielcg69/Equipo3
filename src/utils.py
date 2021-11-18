8f892fcbce04988568830284d5103c255ffc5b6f
uriel <urielsplenser@gmail.com>
2021-11-17 21:46:28 -0300

    Agregado "modificar_string"

--- src/utils.py
+++ src/utils.py
@@ -3,3 +3,13 @@ def cambia_espacios(cadena):
         if cadena[i]==" ":
             cadena=cadena.replace(" ","-")
     return cadena
+
+def modificar_string(palabra, indice, letra):
+    resultado = ''
+    contador = 0
+    for x in palabra:
+        contador += 1
+        if contador == indice:
+            resultado += letra
+        else: resultado += x
+    return resultado
