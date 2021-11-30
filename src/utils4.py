332d936e67b8b83c8cfed78f9867a627fb47aa25
uriel <urielsplenser@gmail.com>
2021-11-29 22:18:59 -0300

    Modifique clases Carrito y Storage

--- src/utils4.py
+++ src/utils4.py
@@ -4,47 +4,70 @@
 #administrador y reportero (solo tiene lectura de datos).El usuario tiene objeto carrito de compras.El administrador puede ver 
 # a todos los usuarios y lo que tenga adentro.El reporter solo ve todos los carritos de compra.
 
-class carrito: #clase que representa a los carritos q son atributos de los usuarios
+class Carrito(): #clase que representa a los carritos q son atributos de los usuarios
     capacidad = 0
     
-    def __init__(self,tamaño) -> None:
+    def __init__(self, tamaño=5) -> None:
         self.capacidad = tamaño
         self.carrito = []
     
-    def AgregaItem(self,item):
-        if (len(carrito) == self.capacidad):
+    def agregar_item(self, item):
+        if (len(self.carrito) == self.capacidad):
             print("El carrito esta completo")
         else:
             self.carrito.append(item)
         
-    def BorraItem(self,item):
-        if len(carrito) == 0:
+    def borrar_item(self, item):
+        if len(self.carrito) == 0:
             print("El carrito no tiene ningún item")
         else:
             self.carrito.remove(item)
+            
+ejemplo1 = Carrito()
+ejemplo1.agregar_item('Manzana')
+ejemplo1.agregar_item('Pera')
+ejemplo1.agregar_item('Banana')
+print(ejemplo1.carrito)
+ejemplo1.borrar_item('Manzana')
+print(ejemplo1.carrito)
 
-class Storage: #lista de los carritos de los usuarios. Es el almacén
-    Almacen = [carrito]
-    Ocupacion = 0
-    def __init__(self) -> None:
-        self.Almacen = [carrito]
+ejemplo2 = Carrito()
+ejemplo2.agregar_item('Olla')
+ejemplo2.agregar_item('Cacerola')
+print(ejemplo2.carrito)
 
-    def cantidadItems(self) -> int:
-        self.Ocupacion = len(self.Almacen)
-        return self.Ocupacion
+class Storage(): #lista de los carritos de los usuarios. Es el almacén
+    almacen = []
+    ocupacion = 0
     
-    def agregaCarrito(self,carrito) -> None:
-        self.Almacen.append(carrito)
-        self.Ocupacion=self.Ocupacion + 1
+    def __init__(self, carrito) -> None:
+        self.almacen = [carrito]
+
+    def cantidad_items(self) -> int:
+        self.ocupacion = len(self.almacen)
+        return self.ocupacion
     
-    def sacaCarrito(self,itm=carrito) -> None:
-        self.Almacen.remove(itm)
-        self.Ocupacion=self.Ocupacion - 1
+    def agregar_carrito(self, carrito) -> None:
+        self.almacen.append(carrito)
+        self.ocupacion += 1
+    
+    def sacar_carrito(self, carrito) -> None:
+        self.almacen.remove(carrito)
+        self.ocupacion -= 1
     
     def __str__(self) -> str:
-        cadena = self.Almacen
-        return cadena + self.Ocupacion
+        cadena = ""
+        for carrito in self.almacen:
+            cadena += "\nCarrito:"
+            for item in carrito.carrito:
+                cadena += " -" + item
+        return cadena
 
+stor1 = Storage(ejemplo1)
+stor1.agregar_carrito(ejemplo2)
+print(stor1)
+
+"""
 class UserTemplate:  #clase abstracta
     Perfil=""
     
@@ -142,3 +165,4 @@ class UserCrate(UserTemplate):
             Usuario(uname,email,URiAvatar,stor)
         else:
             print("Tipo de usuario incorrecto")
+"""
